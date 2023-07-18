01616c459015107c0a9b3b264fc890cb76f95cfb
Colin Eves <it-colin@zincmedia.co.uk>
2023-07-18 10:48:39 +0100

    first commit

---
+++ terms-and-conditions/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <title>Terms and Conditions</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	<link href="css/style.css" rel="stylesheet" media="screen">
+	<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrapValidator.min.js"></script>
+  </head>
+  <body>
+    <div id="outer">
+		<div class="logo" align="center">
+			<img src="images/riba-zincmedia.png">
+		</div>
+		<div class="container">
+		<!-- Content Start -->
+		<h1>T&amp;C's RIBA Directories</h1>
+
+		<h2>Terms of Business</h2>
+		<p>In order to enable Zinc to process this order/case study on your behalf then please ensure that you have read and understood these Terms of Business and ensure that by entering into this agreement you are in a position to comply fully with the requirements as an advertiser/practice.</p>
+			<ol>
+				<li>All Advertiser/case study materials for publication within this Premium Media Package must be provided to Zinc Media in accordance with the current technical requirements and its content must comply with the current guidelines of the Advertising Standards Authority.</li>
+
+				<li>Zinc Media reserves the right</li>
+				<ol type="A">
+					<li>To position and/or to change the size of any advertisement/case study unless such position or size is specifically agreed on this order.</li>
+					<li>To reject, remove or suspend any advertisement/case study or to modify the content/copy of the advertisement.</li>
+					<li>To include an advertisement/case study reference number on all artwork they publish or broadcast.</li>
+				</ol>
+
+				<li>The Advertiser/Practice warrants to Zinc Media that</li>
+				<ol type="A">
+					<li>It has all necessary rights in the advertisement/case study and/or video and its contents.</li>
+					<li>The advertisement/case study and/or video do not contravene any law, regulation or code of practice nor infringe any rights of any third party.</li>
+					<li>The advertisement complies with the Advertising Standards Authority's current code of practice and with all other requirements of any relevant authority.</li>
+					<li>Whereby a video is supplied, it complies with the Advertising Standards Authority’s current codes including both non-broadcast and broadcast codes of practice. Zinc media assumes no liability for the content or images of the video.</li>
+					<li>Insofar as the Advertiser/Practice requires any consent from any third party in connection with the advertisement and/or video and its contents, it has obtained all such consents.</li>
+				</ol>
+
+				<li>The Practice grants Zinc Media a worldwide non-exclusive fully paid licence to use and reproduce and display the practice materials including any artwork, video, trademarks, brand features and any other content contained in the case study submission for the sole purpose of creating a case study in the RIBA Directories. </li>
+
+				<li>Zinc media accepts no liability for any movement in the date of publication. It is hereby agreed that any alteration of publication date will not constitute a breach of this contract.</li>
+
+				<li>All original hard copy artwork provided by the Advertiser/Practice shall remain the property of the Advertiser/Practice and remain at the Advertiser's/Practice’s risk. Zinc Media does not accept liability for any damage or loss whilst in its custody nor during transit.</li>
+
+				<li>The Advertiser/Practice is solely responsible for supplying suitable copy (including artwork which may be required to be reproduced) to Zinc Media within fourteen days of this agreement or by a predefined date.</li>
+
+				<li>Under the terms of this agreement the term ’publish’ refers to Zinc Media’s methods of producing publications including but not limited to print, eBook and digital and social media formats.	</li>
+
+				<li>In the event of Zinc Media not receiving suitable advertising copy, Zinc Media reserve the right to produce artwork on behalf of the Advertiser/practice. In producing such artwork Zinc media will publish either;</li>
+				<ol type="A">
+					<li>The name, address, telephone number and basic business category details of the Advertiser/Practice (if supplied), or</li>
+					<li>A previous advertisement published on behalf of the Advertiser by the Publisher, and in respect of any case Zinc Media will not be responsible for any error or omission arising there from.  </li>
+				</ol>
+
+				<li>In the event of Zinc Media not receiving suitable case study copy, Zinc Media reserve the right to remove the case study from the Publication.</li>
+
+				<li>Zinc Media will periodically notify the practice/advertiser of any sum(s) paid in error to Zinc Media within 12 months of any such payment(s) being made. After 12 months of the date of such payment(s), Zinc Media will not provide any further notification and will not be liable for any refund; instead any sums will belong absolutely to Zinc Media</li>
+
+				<li>If Zinc Media fails to publish the advertisement/case study in accordance with the terms of this agreement the sole liability of Zinc Media to the Advertiser/Practice shall be limited to (at Zinc Media’s option) either a refund of the fee or placement of the advertisement/case study at a later time in a comparable position or publication. Furthermore acceptance by the Publisher of this order form is conditional upon permission being given by the RIBA for the advertisement/case study (or any part thereof) to appear in the publication </li>
+
+				<li>The Advertiser/Practice shall indemnify and keep indemnified Zinc Media against all expenses, damages and losses of any kind incurred by Zinc Media in connection with any claim arising from the advertisement/case study and/or video.</li>
+
+				<li>Payment is strictly due in full within fourteen days of the date of invoice. Invoices are normally issued within fourteen days from the date of order. (For Advertisers only)</li>
+
+				<li>Zinc Media reserves the right to charge a late payment fee in compliance with the Late Payment of Commercial Debts (Interest) Act 1998 (For advertisers only)</li>
+
+				<li>Cancellation of this agreement is conditional on the Advertiser making a written request to Zinc Media which must be received within fourteen days from date of agreement. Cancellation requests should be addressed to Customer Services, Ten Alps Communications Ltd, Kings House, Royal Court, SK11 7AE. Cancellation requests received outside of the 14 day cooling off period will be authorised at the discretion of Zinc Media, and incur a 20% administration  fee payable by the advertiser within 14 days of a cancellation approval code being issued.</li>
+
+				<li>This agreement shall be governed by the laws of the United Kingdom and Ireland under the relevant jurisdiction of England and Wales, Northern Ireland, Scotland or Ireland.</li>
+			</ol>
+
+		<!-- Content End -->
+		</div>
+		<div id="footer" align="center">
+<!--			<h1>2014 All rights reserved. The copyright of council profiles, descriptions, contact information and contact data is owned by LABC and it may not be reproduced without the express permission of LABC.</h1>-->
+		</div>
+	</div>
+	
+  </body>
+</html>
+
+
+
